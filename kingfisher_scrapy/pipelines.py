5175127e5bf977381764ad6f679e5bdcf245c112
rhiaro <amy@rhiaro.co.uk>
2018-12-19 20:14:42 +0200

    Investigating files pipeline extension

--- kingfisher_scrapy/pipelines.py
+++ kingfisher_scrapy/pipelines.py
@@ -5,7 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import logging
+
+from scrapy.pipelines.files import FilesPipeline
+
+
+logger = logging.getLogger(__name__)
+
 
 class KingfisherScrapyPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class KingfisherFilesPipeline(FilesPipeline):
+
+    def file_path(self, request, response=None, info=None):
+        url = request.url
+        media_guid = hashlib.sha1(to_bytes(url)).hexdigest()
+        media_ext = os.path.splitext(url)[1]
+        logger.debug(info)
+        return 'test/%s%s' % (media_guid, media_ext)
