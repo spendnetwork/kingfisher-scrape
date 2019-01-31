5566f7148d614f6e57d464d9eeb9c13d890bf619
James Baster <james.baster@opendataservices.coop>
2019-01-31 09:10:32 +0000

    Don't store local files named with a MD5 https://github.com/open-contracting/kingfisher-scrape/issues/45

--- kingfisher_scrapy/spiders/canada_buyandsell.py
+++ kingfisher_scrapy/spiders/canada_buyandsell.py
@@ -7,16 +7,29 @@ class CanadaBuyAndSell(scrapy.Spider):
 
     def parse(self, response):
         urls = [
-            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-13-14.json',
-            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-14-15.json',
-            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-15-16.json',
-            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-16-17.json',
+            {
+                "file_urls": ['https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-13-14.json'],
+                "data_type": "release_package",
+                "filename": "13-14.json",
+            },
+            {
+                "file_urls": ['https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-14-15.json'],
+                "data_type": "release_package",
+                "filename": "14-15.json",
+            },
+            {
+                "file_urls": ['https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-15-16.json'],
+                "data_type": "release_package",
+                "filename": "15-16.json",
+            },
+            {
+                "file_urls": ['https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-16-17.json'],
+                "data_type": "release_package",
+                "filename": "16-17.json",
+            },
         ]
         if hasattr(self, 'sample') and self.sample == 'true':
-            urls = ['https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-13-14.json']
+            urls = [ urls[0] ]
 
         for url in urls:
-            yield {
-                "file_urls": [url],
-                "data_type": "release_package"
-            }
+            yield url
