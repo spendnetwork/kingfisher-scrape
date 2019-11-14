3272f7759e61602455e1e0e3cfad2bca4e9539db
SimKennedy <simkennedy1@gmail.com>
2019-11-14 12:43:46 +0100

    Fix chile_compra_records

--- kingfisher_scrapy/spiders/chile_compra_records.py
+++ kingfisher_scrapy/spiders/chile_compra_records.py
@@ -26,19 +26,22 @@ class ChileCompraRecords(BaseSpider):
             return
         current_year = datetime.datetime.now().year
         current_month = datetime.datetime.now().month
-        earliest_year = 2007
+        earliest_year = 2008
         if hasattr(self, 'year'):
             current_year = int(self.year)
             earliest_year = current_year - 1
             current_month = 12
-        for year in range(current_year, earliest_year, -1):
+        for year in range(current_year, earliest_year-1, -1):
             for month in range(12, 0, -1):
                 # skip months later than current
                 if current_year == year and month > current_month:
                     continue
+                limit = 1000
+                offset = 0
+                url = 'https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/{}/{:02d}?offset={}&limit={}'.format(year, month, offset, limit)
                 yield scrapy.Request(
-                    url='https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/{}/{:02d}'.format(year, month),
-                    meta={'kf_filename': 'year-{}-month-{:02d}.json'.format(year, month)}
+                    url=url,
+                    meta={'kf_filename': 'year-{}-month-{:02d}-{}-{}.json'.format(year, month, offset, limit)}
                 )
 
     def parse(self, response):
@@ -46,16 +49,34 @@ class ChileCompraRecords(BaseSpider):
         if response.status == 200:
             data = json.loads(response.body_as_unicode())
             if 'NumeroError' in data:
+                # Left in from before, not sure if error has changed
                 yield scrapy.Request(
                     url=response.request.url,
                     meta={'kf_filename': response.request.meta['kf_filename']}
                 )
-            elif 'ListadoOCDS' in data.keys():
-                for data_item in data['ListadoOCDS']:
+            elif 'data' in data.keys():
+                total = data['pagination']['total']
+                offset = data['pagination']['offset']
+                limit = data['pagination']['limit']
+                year = response.url.split('/')[-2]
+                month = response.url.split('/')[-1].split('?')[0]
+
+                for data_item in data['data']:
                     yield scrapy.Request(
-                        url=record_url % data_item['Codigo'].replace('ocds-70d2nz-', ''),
-                        meta={'kf_filename': 'data-%s-record.json' % data_item['Codigo']}
+                        url=record_url % data_item['ocid'].replace('ocds-70d2nz-', ''),
+                        meta={'kf_filename': 'data-%s-record.json' % data_item['ocid']}
                     )
+
+                    # Load more pages?
+                    if not self.is_sample():
+                        new_offset = offset + limit
+                        while new_offset < total:
+                            url = 'https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/{}/{}?limit={}&offset={}'.format(year, month, limit, new_offset)
+                            yield scrapy.Request(
+                                url=url,
+                                meta={'kf_filename': 'year-{}-month-{}-{}-{}.json'.format(year, month, new_offset, limit)}
+                            )
+                            new_offset += limit
 
             else:
                 yield self.save_response_to_disk(response, response.request.meta['kf_filename'], data_type='record_package')
